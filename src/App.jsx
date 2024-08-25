2bb8f2c6e1b96da790d3d71b33ada59559e161c5
santiR22 <santiagorodas688@gmail.com>
2024-08-24 21:14:03 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import React from "react";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <div className="px-4 sm:px-[5vw]"></div>;
 };
 
 export default App;
